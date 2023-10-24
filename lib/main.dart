84954e95b49a7bd266b49508b83bfdbd6d79cd2e
abdybekusyimyk <abdybekuulusyimyk@gmail.com>
2023-10-24 12:42:44 +0600

    anition

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  bool anibool = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    anibool = !anibool;
+                  });
+                },
+                child: const Text("Ontap")),
+            const SizedBox(height: 30),
+            AnimatedOpacity(
+              duration: Duration(milliseconds: 30),
+              opacity: anibool ? 1.0 : 0.1,
+              child: Container(
+                width: 300,
+                height: 300,
+                color: Colors.teal,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
